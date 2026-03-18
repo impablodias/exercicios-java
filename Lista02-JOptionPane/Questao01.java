9d9ef44c4208f0212d887cc2acd6f200cea3987c
Pablo Dias <pabl_dias@icloud.com>
2026-03-17 22:08:02 -0300

    Create Questao01.java
    Fix: Corrigindo nome do arquivo (removendo acentuação) e extensões .java. Finalmente rodando! 🚀

---
+++ Lista02-JOptionPane/Questao01.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class Questao01 {
+    public static void main(String[] args) {
+        String input1 = JOptionPane.showInputDialog("Digite o primeiro número:");
+        String input2 = JOptionPane.showInputDialog("Digite o segundo número:");
+
+        double num1 = Double.parseDouble(input1);
+        double num2 = Double.parseDouble(input2);
+
+        double soma = num1 + num2;
+        double subtracao = num1 - num2;
+        double multiplicacao = num1 * num2;
+        double divisao = num1 / num2;
+
+        String mensagem = "Resultados das operações:\n" +
+                            "Soma: " + soma + "\n" +
+                            "Subtração: " + subtracao + "\n" +
+                            "Multiplicação: " + multiplicacao + "\n" +
+                            "Divisão: " + divisao + "\n";
+
+        JOptionPane.showMessageDialog(null, mensagem);
+
+    }
+}
